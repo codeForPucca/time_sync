b6270d7514858052a8c99271016b5310bacf4d5d
sandrab <sandrab@sandrab.sandrab>
2025-06-07 08:53:01 +0200

    slave: update signal handling, and added gpio pinning

--- slave_sync.c
+++ slave_sync.c
@@ -11,19 +11,53 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <errno.h>
+#include <pigpio.h>
+#include <signal.h>
+
+#ifdef ENABLE_DEBUG
+#define DEBUG_PRINT(...) printf(__VA_ARGS__)
+#else
+#define DEBUG_PRINT(...) do {} while (0)
+#endif
+
 
 // ---- Global Configuration ----
 #define SYNC_PORT 5000
 #define SYNC_BUF_SIZE 8 // one uint64_t
 #define MICROTICK_INTERVAL_NS 1000000 // 1 ms
 #define MICROTICKS_PER_UNIT 20        // 20 ms = 1 logical time unit
+#define SYNCPIN 24
+#define TICKPIN 23
+#define PIN_HIGH_LONG 2
+#define PIN_HIGH_SHORT 1
+
 
 // ---- Global time variable (atomic) ----
 atomic_uint_least64_t global_time = 0;
+int sockfd;
+atomic_int sync_status = 0;           
+atomic_uint_least64_t last_sync_time = 0;      // time since last update from master
+
+void handle_interrupt(int sig) {
+    DEBUG_PRINT("\nInterrupted. Cleaning up...\n");
+    if (sockfd != -1) {
+        close(sockfd);
+    } 
+    gpioWrite(SYNCPIN, 0);
+    gpioWrite(TICKPIN, 0);
+    gpioTerminate();
+    exit(1);
+}
+
+void gpio_pulse_ms(unsigned gpio, unsigned duration_ms) {
+    gpioWrite(gpio, 1);
+    // gpioDelay-> Delay for a number of microseconds -> we want milliseconds
+    gpioDelay(duration_ms * 1000); 
+    gpioWrite(gpio, 0);
+}
 
 // ---- UDP synchronization thread ----
 void *udp_sync_thread(void *arg) {
-    int sockfd;
     struct sockaddr_in servaddr;
     uint64_t remote_tick;
 
@@ -50,15 +84,28 @@ void *udp_sync_thread(void *arg) {
         pthread_exit(NULL);
     }
 
-    printf("Listening for time sync packets on UDP port %d...\n", SYNC_PORT);
+    DEBUG_PRINT("Listening for time sync packets on UDP port %d...\n", SYNC_PORT);
 
     while (1) {
         ssize_t n = recv(sockfd, &remote_tick, sizeof(remote_tick), 0);
         if (n == sizeof(remote_tick)) {
             uint64_t local = atomic_load(&global_time);
             if (remote_tick > local + 1 || remote_tick + 1 < local) {
-                printf("Time correction: local=%lu, received=%lu → updating\n", local, remote_tick);
+                DEBUG_PRINT("Time correction: local=%lu, received=%lu → updating\n", local, remote_tick);
                 atomic_store(&global_time, remote_tick);
+                atomic_store(&last_sync_time, remote_tick);
+                 if (atomic_load(&sync_status) == 0){
+                gpioWrite(SYNCPIN, 1);
+                atomic_store(&sync_status, 1);
+                DEBUG_PRINT("in sync again");}
+
+            }
+            else {
+                DEBUG_PRINT("in sync");
+                atomic_store(&last_sync_time, local);
+                if (atomic_load(&sync_status) == 0){
+                gpioWrite(SYNCPIN, 1);
+                atomic_store(&sync_status, 1);}
             }
         } else {
             perror("Invalid sync packet");
@@ -69,8 +116,31 @@ void *udp_sync_thread(void *arg) {
     return NULL;
 }
 
+
 // ---- Main program with microtick timer ----
 int main() {
+
+     uint64_t new_time = 0;
+     uint64_t last_time = 0;
+
+    //Initialze GPIO
+    if (gpioInitialise() < 0) {
+    fprintf(stderr, "pigpio init failed\n");
+    exit(EXIT_FAILURE);
+    }
+    gpioSetMode(TICKPIN, PI_OUTPUT);
+    gpioWrite(TICKPIN, 0);
+    gpioSetMode(SYNCPIN, PI_OUTPUT);
+    gpioWrite(SYNCPIN, 0);
+
+    // remove GPIO handler
+    gpioSetSignalFunc(SIGINT, NULL);
+    gpioSetSignalFunc(SIGTERM, NULL);
+
+    // Register own handler
+    signal(SIGINT, handle_interrupt);
+    signal(SIGTERM, handle_interrupt);
+
     // Start UDP synchronization thread
     pthread_t udp_thread;
     if (pthread_create(&udp_thread, NULL, udp_sync_thread, NULL) != 0) {
@@ -99,7 +169,7 @@ int main() {
     uint64_t expirations;
     unsigned int microtick_counter = 0;
 
-    printf("Microtick timer started (1ms interval)...\n");
+    DEBUG_PRINT("Microtick timer started (1ms interval)...\n");
 
     while (1) {
         // Block until the timer expires (1ms tick or multiple if delayed)
@@ -113,8 +183,23 @@ int main() {
             microtick_counter++;
             if (microtick_counter >= MICROTICKS_PER_UNIT) {
                 microtick_counter = 0;
-                atomic_fetch_add(&global_time, 1);
-                printf("Global time incremented: %lu\n", atomic_load(&global_time));
+                new_time=atomic_fetch_add(&global_time, 1)+1;
+
+                DEBUG_PRINT("Global time incremented: %lu\n", new_time);
+
+                last_time = atomic_load(&last_sync_time);
+                //atomic_load(&sync_status) == 1 && 
+                if (new_time - last_time > 100) {  // 1macrotick is 20ms * 100 = 2 sec
+                    DEBUG_PRINT("Desync: >2s no new time from master 0\n");
+                    gpioWrite(SYNCPIN, 0);
+                    atomic_store(&sync_status, 0);
+                }
+
+                if (new_time % 10 == 0) {
+                 gpio_pulse_ms(SYNCPIN, PIN_HIGH_LONG);
+                 } else {
+                gpio_pulse_ms(SYNCPIN, PIN_HIGH_SHORT);
+                }
             }
         }
     }
